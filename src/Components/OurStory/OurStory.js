c750aa5e7f96d806043408a198ae3e9e3b791c8a
meganmfisher <meganmichellefisher@gmail.com>
2017-12-16 20:49:42 -0700

    notes

--- src/Components/OurStory/OurStory.js
+++ src/Components/OurStory/OurStory.js
@@ -4,7 +4,11 @@ import './ourstory.css';
 export default function OurStory() {
   return (
     <div className='mainBody'>
+
+      
       
     </div>
   )
 }
+
+//parallax images, about us as a couple and individually. 
